b8b6a4cd3ab1f7d5c6c7708f008c2a9fc55796e5
Eduardo Encarnacao <eduardo.encarnacao@outlook.com>
2020-08-31 15:40:51 -0300

    Adicionado Funcionario Service e testes unitários dos mesmos.

---
+++ src/main/java/com/kazale/pontointeligente/api/services/impl/FuncionarioServiceImpl.java
@@ -0,0 +1,45 @@
+package com.kazale.pontointeligente.api.services.impl;
+
+import com.kazale.pontointeligente.api.entities.Funcionario;
+import com.kazale.pontointeligente.api.repositories.FuncionarioRepository;
+import com.kazale.pontointeligente.api.services.FuncionarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class FuncionarioServiceImpl implements FuncionarioService {
+
+    private static final Logger log = LoggerFactory.getLogger(FuncionarioService.class);
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Override
+    public Funcionario persistir(Funcionario funcionario) {
+        log.info("Persistindo funcionário: {}", funcionario);
+        return this.funcionarioRepository.save(funcionario);
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorCpf(String cpf) {
+        log.info("Buscando funcionário pelo CPF {}", cpf);
+        return Optional.ofNullable(this.funcionarioRepository.findByCpf(cpf));
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorEmail(String email) {
+        log.info("Buscando funcionário pelo email {}", email);
+        return Optional.ofNullable(this.funcionarioRepository.findByEmail(email));
+    }
+
+    @Override
+    public Optional<Funcionario> buscarPorId(Long id) {
+        log.info("Buscando funcionário pelo ID {}", id);
+        return this.funcionarioRepository.findById(id);
+    }
+
+}
