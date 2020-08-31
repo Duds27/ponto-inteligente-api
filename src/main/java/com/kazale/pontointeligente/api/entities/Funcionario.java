397c8a703b3c6a9fc56a410b119b759950654653
Eduardo Encarnacao <eduardo.encarnacao@outlook.com>
2020-08-31 13:05:05 -0300

    Adicionado camada de Repository e testes unitários dos mesmos.

--- src/main/java/com/kazale/pontointeligente/api/entities/Funcionario.java
+++ src/main/java/com/kazale/pontointeligente/api/entities/Funcionario.java
@@ -78,7 +78,7 @@ public class Funcionario implements Serializable {
         this.cpf = cpf;
     }
 
-    @Column(name = "valor_hora", nullable = false)
+    @Column(name = "valor_hora", nullable = true)
     public BigDecimal getValorHora() {
         return valorHora;
     }
@@ -92,7 +92,7 @@ public class Funcionario implements Serializable {
         this.valorHora = valorHora;
     }
 
-    @Column(name = "qtd_horas_trabalho_dia", nullable = false)
+    @Column(name = "qtd_horas_trabalho_dia", nullable = true)
     public Float getQtdHorasTrabalhoDia() {
         return qtdHorasTrabalhoDia;
     }
@@ -106,7 +106,7 @@ public class Funcionario implements Serializable {
         this.qtdHorasTrabalhoDia = qtdHorasTrabalhoDia;
     }
 
-    @Column(name = "qtd_horas_almoco", nullable = false)
+    @Column(name = "qtd_horas_almoco", nullable = true)
     public Float getQtdHorasAlmoco() {
         return qtdHorasAlmoco;
     }
